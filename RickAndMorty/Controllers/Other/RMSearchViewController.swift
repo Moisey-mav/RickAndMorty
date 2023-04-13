7bea3e8e531d6de4a4535e42e3278da6167f5ae8
Vladisalv Moiseev <80466128+Moisey-mav@users.noreply.github.com>
2023-04-13 18:26:20 +0300

    Added no search results view

--- RickAndMorty/Controllers/Other/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/RMSearchViewController.swift
@@ -31,12 +31,16 @@ final class RMSearchViewController: UIViewController {
         let type: `Type`
     }
     
-    private let config: Confog
+    private let viewModel: RMSearchViewViewModel
+    
+    private let searchView: RMSearchView
     
     // MARK: - Init
     
     init(config: Confog) {
-        self.config = config
+        let viewModel = RMSearchViewViewModel(config: config)
+        self.viewModel = viewModel
+        self.searchView = RMSearchView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -48,9 +52,27 @@ final class RMSearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = config.type.title
+        title = viewModel.config.type.title
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(searchView)
+        addConstraints()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Search", style: .done, target: self, action: #selector(didTapExecuteSearch))
     }
     
+    @objc func didTapExecuteSearch() {
+//        viewModel.executeSearch()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            searchView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+        ])
+    }
 
 }
