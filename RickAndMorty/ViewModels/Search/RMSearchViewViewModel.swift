4480dbb2cd341f0799a213c9b59d6f2ea26c46fc
Vladisalv Moiseev <80466128+Moisey-mav@users.noreply.github.com>
2023-04-13 17:05:23 +0300

    Start building search

---
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RMSearchViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Владислав Моисеев on 13.04.2023.
+//
+
+import Foundation
+
+final class RMSearchViewViewModel {
+    let config: RMSearchViewController.Confog
+    
+    init(config: RMSearchViewController.Confog) {
+        self.config = config
+    }
+}
---
+++ RickAndMorty/Views/Search/RMNoSearchResultsView.swift
@@ -0,0 +1,13 @@
+//
+//  RMNoSearchResultsView.swift
+//  RickAndMorty
+//
+//  Created by Владислав Моисеев on 13.04.2023.
+//
+
+import UIKit
+
+final class RMNoSearchResultsView: UIView {
+   
+
+}
