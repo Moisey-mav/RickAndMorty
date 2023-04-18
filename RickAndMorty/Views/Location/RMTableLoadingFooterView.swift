b433bc965da79cc6981f3a7985b33c502e793096
Vladisalv Moiseev <80466128+Moisey-mav@users.noreply.github.com>
2023-04-18 17:04:13 +0300

    Wrap up location pagination + device families

---
+++ RickAndMorty/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by Владислав Моисеев on 18.04.2023.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(spinner)
+        spinner.startAnimating()
+        
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+}
